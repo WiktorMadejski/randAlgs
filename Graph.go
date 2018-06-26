5efc7af33e96f81c393fc1d0a16d8a5b0cd44641
Damian Skrzypiec <=>
2018-06-26 21:09:42 +0200

    Add AddRow function and CreateEmpty

--- Graph.go
+++ Graph.go
@@ -1,20 +1,35 @@
 package randAlgs
 
+import "log"
+
 // IndMatrix represents a graph as incidence matrix.
-type IndMatrix  struct {
-	values []int
-	dim int
+type IndMatrix struct {
+	Dim    int
+	Values []int // dim * dim values expected
+}
+
+// CreateEmptyIndMatrix returns empty IndMatrix of given dimension.
+func CreateEmptyIndMatrix(dim int) IndMatrix {
+	values := make([]int, 0, dim)
+	return IndMatrix{dim, values}
+}
+
+func (im *IndMatrix) AddRow(values []int) {
+	if len(values) != im.Dim {
+		log.Printf("[AddRow] Expected %d values, got %d \n.", im.Dim, len(values))
+	}
+
+	for _, e := range values[0:im.Dim] {
+		im.Values = append(im.Values, e)
+	}
 }
 
 // Returns i-th row of IndMatrix.
-func (im *IndMatrix) GetRow(i int) []int {
+func (im *IndMatrix) GetRowValues(i int) []int {
 	return make([]int, 0)
 }
+
 // Returns i-th col of IndMatrix.
-func (im *IndMatrix) GetCol(i int) []int {
+func (im *IndMatrix) GetColValues(i int) []int {
 	return make([]int, 0)
-}
-
-func CreateEmptyIndMatrix() IndMatrix {
-
 }
