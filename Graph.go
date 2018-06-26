e1f2201ffb7dfef620717187a8a7b2eb534f40d7
wmade <w.madejski@gmail.com>
2018-06-26 20:43:21 +0200

    Add draft of Graph definition
            (cherry picked from commit 650df7f)

--- Graph.go
+++ Graph.go
@@ -7,7 +7,6 @@ type IndMatrix []int
 func (im *IndMatrix) GetRow(i int) []int {
 	return make([]int, 0)
 }
-
 // Returns i-th col of IndMatrix.
 func (im *IndMatrix) GetCol(i int) []int {
 	return make([]int, 0)
